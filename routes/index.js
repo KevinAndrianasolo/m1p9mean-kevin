da874a843cb66001355b9449597464d5d1c2b9f5
Kevin Andrianasolo <kevin.andrianasolo.lala@gmail.com>
2022-03-24 08:19:39 +0300

    Dist client app

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,6 @@ var appDir = path.resolve(__dirname, '..');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //res.render('index', { title: 'Express' });
-  console.log(__dirname);
-  console.log(appDir);
   let indexPath = path.resolve(appDir, "m1p9mean-kevin-client/dist/m1p9mean-kevin-client/index.html");
   res.sendFile(indexPath);
 });
