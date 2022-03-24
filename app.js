f03593b233a0ef87e04faf2c46ebe337092cee13
Kevin Andrianasolo <kevin.andrianasolo.lala@gmail.com>
2022-03-24 16:33:10 +0300

    Update app.js

--- app.js
+++ app.js
@@ -38,8 +38,12 @@ app.use('/auth', authRouter);
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+
+app.all('*', function(req, res) { 
+  res.redirect('/'); 
 });
 
 // error handler
