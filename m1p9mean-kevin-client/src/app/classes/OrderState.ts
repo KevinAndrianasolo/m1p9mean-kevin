2a6a36ed9d062d151bff7adc4bbf04921f1c598f
Kevin Andrianasolo <kevin.andrianasolo.lala@gmail.com>
2022-04-02 10:11:43 +0300

    OrderState Fix

--- m1p9mean-kevin-client/src/app/classes/OrderState.ts
+++ m1p9mean-kevin-client/src/app/classes/OrderState.ts
@@ -3,4 +3,11 @@ export class OrderState{
     static READY = "6242cddc2597694c914ff9f0";
     static RESERVED_BY_DELIVERY_MAN = "6242cdeb2597694c914ff9f1";
     static DELIVERED_AND_PAID = "6242a6cf2597694c914ff9eb";
+
+    static map : any = {
+        "6242a6c52597694c914ff9ea" : "En cours",
+        "6242cddc2597694c914ff9f0" : "Prête à être livrée",
+        "6242cdeb2597694c914ff9f1" : "Prise en charge par le livreur",
+        "6242a6cf2597694c914ff9eb" : "Livrée et Payée"
+    }
 };
