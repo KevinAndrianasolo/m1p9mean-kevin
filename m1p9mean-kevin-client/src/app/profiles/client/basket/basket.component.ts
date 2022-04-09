874fcbdf8ba63f97647ff89895640f6a9c4406cf
Kevin Andrianasolo <kevin.andrianasolo.lala@gmail.com>
2022-04-09 07:19:57 +0300

    Corbeille Drag and Drop

--- m1p9mean-kevin-client/src/app/profiles/client/basket/basket.component.ts
+++ m1p9mean-kevin-client/src/app/profiles/client/basket/basket.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { Collections } from 'src/app/classes/Collections';
 import { OrderState } from 'src/app/classes/OrderState';
 import { ApiService } from 'src/app/services/api/api.service';
+import { BasketService } from 'src/app/services/basket/basket.service';
 import { PopupService } from 'src/app/services/popup/popup.service';
 import { StorageService } from 'src/app/services/storage/storage.service';
 
@@ -15,7 +16,7 @@ export class BasketComponent implements OnInit {
   public basket : any []= [];
   public onLoading : boolean = false;
   public basketItems : any[] =[];
-  constructor(private api : ApiService, private storageService : StorageService, private popupService : PopupService, private router : Router) {
+  constructor(private api : ApiService, private storageService : StorageService, private popupService : PopupService, private router : Router, private basketService : BasketService) {
     this.router.routeReuseStrategy.shouldReuseRoute = () => false;
    }
 
@@ -25,12 +26,69 @@ export class BasketComponent implements OnInit {
     this.InitBasketItems();
     console.log(this.basketItems);
   }
+  public onDragLeave($event : DragEvent){
+    $event.preventDefault();
+    let element : any= $event.target;
+    if(element){
+      element.style['box-shadow'] = "none";
+      element.style.color = "black";
+    } 
+  }
+  public onDragOver($event : DragEvent){
+    $event.preventDefault();
+    let element : any= $event.target;
+    if(element) {
+      element.style['box-shadow'] = "0px 0px 20px red";
+      element.style.color = "red";
+    }
+  }
+  public onDragEnd($event : DragEvent){
+    // $event.preventDefault();
+    let element : any= $event.target;
+    if(element) element.style.opacity = 1;
+  }
+  public onDragStart($event : DragEvent, basketItem : any){
+    // $event.preventDefault();
+    console.log(basketItem);
+    let element : any= $event.target;
+    if(element) element.style.opacity = 0.5;
+    if($event.dataTransfer){
+      $event.dataTransfer.effectAllowed = 'move';
+      $event.dataTransfer.setData("text/plain", JSON.stringify({
+        restaurantId : basketItem.restaurantId,
+        menuId : basketItem.menuId,
+
+      }));
+    }
+  }
+  public handleDrop($event : DragEvent) {
+    $event.preventDefault();
+    if($event.dataTransfer){
+      let tmp : string = $event.dataTransfer?.getData('text/plain');
+      let basketItem = JSON.parse(tmp);
+      let basket = this.basketService.getBasketOf(basketItem.restaurantId);
+      if(basket){
+        let iItem = this.basketService.indexOf(basket.items, 'menuId', basketItem.menuId);
+        this.basketService.deleteFromBasket(basketItem.restaurantId, iItem);
+        this.ngOnInit();
+      }
+    }
+    let element : any= $event.target;
+    if(element){
+      element.style['box-shadow'] = "none";
+      element.style.color = "black";
+    } 
+    
+    return false;
+  }
+
   public async InitBasketItems(){
     this.basketItems = [];
     for(let i=0; i<this.basket.length; i++){
       for(let j=0; j<this.basket[i].items.length; j++){
         let item = this.basket[i].items[j];
         item.menu = await this.getMenu(item.menuId);
+        item.restaurantId = this.basket[i].restaurantId;
         this.basketItems.push(item);
       }
     }
